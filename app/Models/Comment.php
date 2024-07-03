7988e3d41bfcc41d5aecc7ceadda610d57e554de
pezhmana <pejman.ardalani2@gmail.com>
2024-07-03 19:49:36 +0330

    add relation for comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -26,6 +26,11 @@ class Comment extends Model
     public function post(){
         return $this->belongsTo(Post::class);
     }
+
+    public function commentable()
+    {
+        return $this->morphTo();
+    }
 }
 
 

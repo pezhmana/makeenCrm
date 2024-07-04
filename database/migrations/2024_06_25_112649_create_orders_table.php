49e1d792ac87851932726222ece9193975767e5a
pezhmana <pejman.ardalani2@gmail.com>
2024-07-04 12:09:59 +0330

    edit order table

--- database/migrations/2024_06_25_112649_create_orders_table.php
+++ database/migrations/2024_06_25_112649_create_orders_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->bigInteger('sum');
             $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('product_id');
             $table->timestamps();
         });
     }

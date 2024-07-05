2fb43fc6279b84b7cf3402b63e305bc77d8bd5d7
pezhmana <pejman.ardalani2@gmail.com>
2024-07-05 13:31:33 +0330

    add relation polymorphic for category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,12 +4,35 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Category extends Model
+class Category extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
+
+    protected $fillable=[
+      'name',
+        'category_id',
+        'categoryable_id',
+        "categoryable_type"
+    ];
 
     public function products(){
-        return $this->hasMany(Product::class);
+        return $this->morphedByMany(Product::class , 'categoryable');
     }
+
+    public function posts(){
+        return $this->morphedByMany(Post::class , 'categoryable');
+    }
+
+    public function category(){
+        return $this->MorphToMany(Category::class , 'categoryable');
+    }
+
+    public function categories(){
+        return $this->morphedByMany(Category::class , 'categoryable');
+    }
+
+
 }

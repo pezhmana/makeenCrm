5f61cd0d88810401aaafaf437986af7a833b63b7
pooya <razavi.blackghost@gmail.com>
2024-07-08 12:41:19 +0330

    add hasmedia model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,16 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Post extends Model
+class Post extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory ,InteractsWithMedia;
     protected $fillable=[
         'name',
         'user_id',
         'short',
         'description',
         'time',
+
         ];
 
     public function comments(){

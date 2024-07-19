a2b9f66ec981031feffc843d4f6eca0219721882
pezhmana <pejman.ardalani2@gmail.com>
2024-07-19 14:07:57 +0330

    add resource to get user index for admin

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -12,10 +13,16 @@ class UserResource extends JsonResource
      *
      * @return array<string, mixed>
      */
+
     public function toArray(Request $request): array
     {
         return [
-            'id'=>$this->id
+            'id'=>$this->id,
+            'full_name'=>$this->name.' '.$this->last_name,
+            'phone'=>$this->phone,
+            'orders_count' => $this->orders->count(),
+            'orders_sum'=>$this->orders->sum('sum'),
+            'latest_order'=>$this->orders->sortByDesc('created_at')->first()->created_at,
         ];
     }
 }

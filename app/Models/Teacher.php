9511ef179de527a5df67abab96cb181a74582d8b
pezhmana <pejman.ardalani2@gmail.com>
2024-07-20 12:37:29 +0330

    add teacher profile image attribute

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Teacher extends Model
+class Teacher extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory , InteractsWithMedia;
     protected $fillable=[
         'id' ,
         'name' ,
@@ -18,7 +20,25 @@ class Teacher extends Model
         'message'
     ];
 
+    protected $appends=[
+        'teacher_image'
+    ];
+
+    protected $hidden=[
+        'media'
+    ];
+
     public function products(){
         return $this->hasMany(Product::class);
+    }
+
+    public function getTeacherImageAttribute(){
+        $image=  $this->getMedia('teacher.image')->last();
+        if($image){
+            $Url = $image->getUrl();
+        }else{
+            $Url=null;
+        }
+        return $Url;
     }
 }

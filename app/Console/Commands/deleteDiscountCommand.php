96fc7c7f0dc8d2431e51d1f1fbc02633ca9990a4
pezhmana <pejman.ardalani2@gmail.com>
2024-07-21 15:00:41 +0330

    add delete discount command

---
+++ app/Console/Commands/deleteDiscountCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Discount;
+use Illuminate\Console\Command;
+
+class deleteDiscountCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'discount:delete';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'delete finished ';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Discount::where('amount',0)->delete();
+    }
+}

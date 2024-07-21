3446d8e391ecc8701de2608fe3d0be5035421ac4
pezhmana <pejman.ardalani2@gmail.com>
2024-07-21 15:03:08 +0330

    crud  discount

--- app/Http/Controllers/DiscountController.php
+++ app/Http/Controllers/DiscountController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Discount;
 use Illuminate\Http\Request;
 
 class DiscountController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+
+        $discount = Discount::create($request->toArray());
+        return response()->json($discount);
+    }
+    public function index($id = null)
+    {
+        if ($id) {
+            $discount = Discount::where('id', $id)->first();
+        } else {
+            $discount = Discount::orderby('id', 'desc')->paginate(10);
+        }
+        return response()->json($discount);
+    }
+
+    public function edit(Request $request, $id)
+    {
+        $discount = Discount::where('id', $id)->update($request->toArray());
+        return response()->json($discount);
+    }
+
+    public function delete($id)
+    {
+        $discount = Discount::where('id', $id)->delete();
+        return response()->json($discount);
+    }
 }

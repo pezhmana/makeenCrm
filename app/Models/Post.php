352a56472da1d27f6681e169273bdca2c3929580
ali <shaltenkoo7576@gmail.com>
2024-07-22 15:08:59 -1200

    modelpost

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory,InteractsWithMedia;
     protected $fillable=[
         'name',
         'user_id',

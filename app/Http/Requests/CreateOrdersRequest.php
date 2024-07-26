c541563782900d670d51cea1a8ad8c226873ab43
pooya <razavi.blackghost@gmail.com>
2024-07-26 20:18:09 +0330

    add create order Request

--- app/Http/Requests/CreateOrdersRequest.php
+++ app/Http/Requests/CreateOrdersRequest.php
@@ -11,7 +11,7 @@ class CreateOrdersRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,7 @@ class CreateOrdersRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'sum'=>'required|integer'
         ];
     }
 }

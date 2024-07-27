5cb33af7ac3f1eebc2fefd05892f6bb8244c32e2
pezhmana <pejman.ardalani2@gmail.com>
2024-07-27 12:34:38 +0330

    add answer comment for admin

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -16,12 +16,14 @@ class CommentController extends Controller
           $comment =Comment::create($request->merge([
               'commentable_type'=>'App\Models\Post',
               'user_id'=>Auth::user()->id,
+              'comment_id'=>0
               ])->toArray());
         }
         if($type == 'product'){
             $comment = Comment::create($request->merge([
                 'commentable_type'=>'App\Models\Product',
                 'user_id'=>Auth::user()->id,
+                'comment_id'=>0
             ])->toArray());
         }
         return response()->json($comment);
@@ -30,6 +32,7 @@ class CommentController extends Controller
     public function index( CreateCommentRequest $request){
         $type = $request->type;
         $comment = new Comment();
+        $comment = $comment->with('comments');
         if($type == 'post'){
             $comment = $comment->where('commentable_type','App\Models\Post');
         }
@@ -46,5 +49,16 @@ class CommentController extends Controller
     public function delete($id){
         Comment::destroy($id);
         return response()->json("کاربر با موفقیت حذف شد");
+    }
+
+    public function answer(Request $request){
+        $comment_id = $request->comment_id;
+        $comment = Comment::create($request->merge([
+            'comment_id'=>$comment_id,
+            'user_id'=>Auth::user()->id,
+            'commentable_type'=>Comment::find($comment_id)->first()->commentable_type,
+            'commentable_id'=>Comment::find($comment_id)->first()->commentable_id
+        ])->toArray());
+        return response()->json($comment);
     }
 }

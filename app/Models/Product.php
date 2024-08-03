e5af39d21ac6cbde725eba8cec371b35f2fb3fe3
pezhmana <pejman.ardalani2@gmail.com>
2024-08-03 11:22:38 +0330

    edit relation

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
@@ -72,6 +73,11 @@ class Product extends Model implements HasMedia
 
     public function ratings(){
         return $this->hasMany(rating::class);
+    }
+
+    public function chapters(): HasMany
+    {
+        return $this->hasMany(Chapter::class);
     }
 }
 

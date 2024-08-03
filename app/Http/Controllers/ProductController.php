f487122d64250053d0e8ce5bae44308f6f4f2ab0
pezhmana <pejman.ardalani2@gmail.com>
2024-08-03 11:23:43 +0330

    add media compelete

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateProductsRequest;
 use App\Models\Category;
+use App\Models\Chapter;
 use App\Models\Comment;
 use App\Models\Order;
 use App\Models\Product;
@@ -12,6 +13,7 @@ use Illuminate\Contracts\Database\Query\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Spatie\MediaLibrary\Conversions\ImageGenerators\Video;
 
 class ProductController extends Controller
 {
@@ -47,11 +49,13 @@ class ProductController extends Controller
             $user =Auth::user()->id;
             $purchase = Order::where('user_id',$user)->where('product_id',$id);
             if($purchase->exists()){
-                $media = Product::find($id)->getMedia('*');
-                $video = [];
-                foreach ($media as $mediaItem) {
-                    $video[] = $mediaItem->getUrl();
-                }
+                $video = Product::find($id)->chapters()->with('videos')->get();
+//                dd($media);
+//                $video = [];
+//                foreach ($media as
+// $mediaItem) {
+//                    $video[] = $mediaItem->url;
+//                }
             }else{
                 $video = 'برای دسترسی باید دوره را خریداری نمایید';
             }
@@ -133,10 +137,24 @@ class ProductController extends Controller
         return response()->json($product);
     }
 
-    public function addmedia($id , Request $request){
-        $product = Product::find($id);
-        $collection = DB::table('collections')->find($request->season);
-        $video =$product->addMediaFromRequest('media')->toMediaCollection($collection->collection);
+    public function addmedia(Request $request,$id){
+        $exist = Chapter::where('title',$request->chapter);
+        if($exist->exists()){
+            $season = $exist->first();
+        }else{
+            $season = Product::find($id)->chapters()->create([
+                'title'=>$request->chapter
+            ]);
+        }
+        $video = $season->videos()->create([
+            'title' => $request->title,
+        ]);
+        $media= $video->addMediaFromRequest('media')->toMediaCollection('product.videos');
+    $url = $media->getUrl();
+    $video->update([
+        'url'=>$url
+    ]);
+//    $response = $season->with('videos');
         return response()->json($video);
     }
 

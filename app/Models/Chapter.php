75f2ebd54d12ffc37a016e44edd8bc49e92e91d8
pezhmana <pejman.ardalani2@gmail.com>
2024-08-03 11:23:03 +0330

    add chapter model

---
+++ app/Models/Chapter.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class Chapter extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+        'product_id',
+        'title'
+    ];
+
+    public function product()
+    {
+       return $this->belongsTo(Product::class);
+    }
+
+    public function videos(){
+        return $this->hasMany(video::class);
+    }
+}

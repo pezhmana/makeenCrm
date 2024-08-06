aca4decd9f226f57663c5330385b2b37274bb577
pezhmana <pejman.ardalani2@gmail.com>
2024-08-06 16:34:58 +0330

    add relation video for user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\DB;
@@ -15,7 +16,7 @@ use Spatie\MediaLibrary\InteractsWithMedia;
 class User extends Authenticatable implements HasMedia
 
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles, InteractsWithMedia;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles, InteractsWithMedia ,SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -84,7 +85,7 @@ class User extends Authenticatable implements HasMedia
     public function orderProducts(): array
     {
         $data = [];
-        $orders = $this->orders()->get();
+        $orders = $this->orders()->orderByDesc('created_at')->get();
         foreach ($orders as $order) {
             $data[] = $order->product;
         }
@@ -131,6 +132,38 @@ class User extends Authenticatable implements HasMedia
 
     public function likes(){
         return $this->hasMany(Like::class);
+    }
+
+    public function videos(){
+        return $this->belongsToMany(video::class)->withTimestamps();
+    }
+
+    public function compelete(){
+        $complete=0;
+        $unComplete=0;
+        $user=auth()->user();
+        $orders =$user->orders()->get();
+        $products=[];
+        foreach ($orders as $order) {
+            $products[] = $order->product;
+        }
+        foreach ($products as $product){
+           $video_count =  $product->videos()->count();
+//           dd($pr);
+           $uservideo_count= DB::table('user_video')
+               ->where('product_id',$product->id)->where('user_id',$user->id)
+               ->count();
+//           dd($uservideo_count);
+           if($uservideo_count == $video_count ){
+               $complete +=1;
+           }else{
+               $unComplete +=1;
+           }
+
+        }
+        return [
+            'complete'=>$complete,
+            'uncomplete'=>$unComplete];
     }
 
 }

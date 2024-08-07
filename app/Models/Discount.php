b2d8038397f7ba18aa98e369210408c415a02cf6
pezhmana <pejman.ardalani2@gmail.com>
2024-08-07 11:23:36 +0330

    add withtrashed

--- app/Models/Discount.php
+++ app/Models/Discount.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Discount extends Model
 {
-    use HasFactory;
+    use HasFactory , SoftDeletes;
     protected $fillable = [
         'amount',
         'code' ,
@@ -20,4 +21,11 @@ class Discount extends Model
     public function orders(){
         return $this->hasMany(Order::class);
     }
+
+    public function getStatusDiscountAttribute(){
+        return is_null($this->deleted_at);
+    }
+    protected $appends=[
+      'Status_discount'
+    ];
 }

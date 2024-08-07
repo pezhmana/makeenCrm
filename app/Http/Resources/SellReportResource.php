c7c30dcfb7de97656908b65c4f9f93401085f84f
pezhmana <pejman.ardalani2@gmail.com>
2024-08-07 15:06:42 +0330

    add deafualt time

--- app/Http/Resources/SellReportResource.php
+++ app/Http/Resources/SellReportResource.php
@@ -16,13 +16,20 @@ class SellReportResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-
+        $from = $request->from;
+        $to = $request->to;
+        if(is_null($request->from)){
+            $from = '2022-01-01';
+        }
+        if(is_null($to)){
+            $to = '2026-01-01';
+        }
         return[
             'id'=>$this->id,
             'product_name'=>$this->name,
-            'teacher_name'=>Teacher::where('id',$this->teacher_id)->first()->name,
-            'order_count'=>$this->orders()->whereBetween('created_at', [$request->from, $request->to])->count(),
-            'order_sum'=>$this->orders()->whereBetween('created_at', [$request->from, $request->to])->sum('sum')
+            'teacher_name'=>Teacher::find($this->teacher_id)->name,
+            'order_count'=>$this->orders()->whereBetween('created_at', [$from, $to])->count(),
+            'order_sum'=>$this->orders()->whereBetween('created_at', [$from, $to])->sum('sum')
 
         ];
     }

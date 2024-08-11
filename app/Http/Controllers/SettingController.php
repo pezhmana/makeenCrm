3bab5d45fdb67515ec4b63b73b99258276aa7097
pezhmana <pejman.ardalani2@gmail.com>
2024-08-11 12:33:48 +0330

    decode json

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers;
 use App\Http\Requests\EditSettingRequest;
 use App\Models\Setting;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use function PHPUnit\Framework\isJson;
 
 class SettingController extends Controller
 {
     public function index($key = null){
         if($key){
             $setting = Setting::where('key', $key)->first();
+            if (Str::isJson($setting->value)) {
+                $setting = json_decode($setting->value, true);
+            }
         }
         else{
             $setting = Setting::paginate(10);
